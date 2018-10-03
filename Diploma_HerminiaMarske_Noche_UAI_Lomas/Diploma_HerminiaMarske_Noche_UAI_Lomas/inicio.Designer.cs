070b90acffbe65f2308b5389b1e16a20fe3888e8
HermiMarske <35698704+HermiMarske@users.noreply.github.com>
2018-10-02 23:19:27 -0300

    Pantalla alta cliente
    controles terminados

--- Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/inicio.Designer.cs
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/inicio.Designer.cs
@@ -77,8 +77,26 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanelAltaCliente.SuspendLayout();
+            this.datosClienteGroup.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -217,29 +235,47 @@
             this.tableLayoutPanelAltaCliente.Controls.Add(this.datosClienteGroup, 0, 0);
             this.tableLayoutPanelAltaCliente.Controls.Add(this.groupBox1, 1, 0);
             this.tableLayoutPanelAltaCliente.Controls.Add(this.groupBox2, 1, 1);
+            this.tableLayoutPanelAltaCliente.Controls.Add(this.button5, 0, 2);
+            this.tableLayoutPanelAltaCliente.Controls.Add(this.button6, 1, 2);
             this.tableLayoutPanelAltaCliente.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelAltaCliente.Location = new System.Drawing.Point(0, 28);
             this.tableLayoutPanelAltaCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanelAltaCliente.Name = "tableLayoutPanelAltaCliente";
-            this.tableLayoutPanelAltaCliente.RowCount = 2;
-            this.tableLayoutPanelAltaCliente.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 353F));
+            this.tableLayoutPanelAltaCliente.RowCount = 3;
+            this.tableLayoutPanelAltaCliente.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 343F));
             this.tableLayoutPanelAltaCliente.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelAltaCliente.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
             this.tableLayoutPanelAltaCliente.Size = new System.Drawing.Size(1304, 634);
             this.tableLayoutPanelAltaCliente.TabIndex = 1;
             this.tableLayoutPanelAltaCliente.Visible = false;
             // 
             // datosClienteGroup
             // 
+            this.datosClienteGroup.Controls.Add(this.dateTimePicker1);
+            this.datosClienteGroup.Controls.Add(this.label12);
+            this.datosClienteGroup.Controls.Add(this.label11);
+            this.datosClienteGroup.Controls.Add(this.comboBox6);
+            this.datosClienteGroup.Controls.Add(this.label8);
+            this.datosClienteGroup.Controls.Add(this.checkBox1);
+            this.datosClienteGroup.Controls.Add(this.textBox12);
+            this.datosClienteGroup.Controls.Add(this.textBox11);
+            this.datosClienteGroup.Controls.Add(this.label10);
+            this.datosClienteGroup.Controls.Add(this.label9);
+            this.datosClienteGroup.Controls.Add(this.textBox9);
+            this.datosClienteGroup.Controls.Add(this.textBox8);
+            this.datosClienteGroup.Controls.Add(this.label7);
+            this.datosClienteGroup.Controls.Add(this.textBox7);
+            this.datosClienteGroup.Controls.Add(this.label6);
             this.datosClienteGroup.Dock = System.Windows.Forms.DockStyle.Fill;
             this.datosClienteGroup.Location = new System.Drawing.Point(4, 4);
             this.datosClienteGroup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.datosClienteGroup.Name = "datosClienteGroup";
             this.datosClienteGroup.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanelAltaCliente.SetRowSpan(this.datosClienteGroup, 2);
-            this.datosClienteGroup.Size = new System.Drawing.Size(608, 626);
+            this.datosClienteGroup.Size = new System.Drawing.Size(608, 577);
             this.datosClienteGroup.TabIndex = 0;
             this.datosClienteGroup.TabStop = false;
-            this.datosClienteGroup.Text = "Datos";
+            this.datosClienteGroup.Text = "Datos de Persona y Empresa";
             // 
             // groupBox1
             // 
@@ -269,7 +305,7 @@
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(680, 345);
+            this.groupBox1.Size = new System.Drawing.Size(680, 335);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Domicilios";
@@ -284,11 +320,11 @@
             this.groupBox2.Controls.Add(this.textBox6);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(620, 357);
+            this.groupBox2.Location = new System.Drawing.Point(620, 347);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(680, 273);
+            this.groupBox2.Size = new System.Drawing.Size(680, 234);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Telefonos";
@@ -336,7 +372,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(447, 34);
+            this.textBox3.Location = new System.Drawing.Point(447, 32);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(225, 22);
             this.textBox3.TabIndex = 5;
@@ -528,6 +564,147 @@
             this.listBox2.Size = new System.Drawing.Size(660, 84);
             this.listBox2.TabIndex = 21;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Nombre";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(72, 32);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(239, 22);
+            this.textBox7.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "CUIT";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(53, 293);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(334, 22);
+            this.textBox8.TabIndex = 3;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(381, 32);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(220, 22);
+            this.textBox9.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(317, 37);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 17);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Apellido";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(8, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 17);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "DNI";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(45, 74);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(266, 22);
+            this.textBox11.TabIndex = 9;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(106, 249);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(281, 22);
+            this.textBox12.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 208);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 21);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Es empresa?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 77);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Sexo";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(362, 74);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(239, 24);
+            this.comboBox6.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(8, 252);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 17);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Razon Social";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(8, 119);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(141, 17);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "Fecha de Nacimiento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(156, 118);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(445, 22);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 588);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(206, 43);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Limpiar Controles";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(619, 588);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(206, 43);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Agregar Cliente";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // formInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -545,6 +722,8 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.tableLayoutPanelAltaCliente.ResumeLayout(false);
+            this.datosClienteGroup.ResumeLayout(false);
+            this.datosClienteGroup.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -605,6 +784,23 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
