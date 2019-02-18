bc33322e4414f9146930039f911248332e44ff88
HermiMarske <35698704+HermiMarske@users.noreply.github.com>
2019-02-18 00:56:32 -0300

    modificar
    eso

--- Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/modificarCliente.Designer.cs
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/modificarCliente.Designer.cs
@@ -159,6 +159,7 @@
             this.btnCancelar.TabIndex = 1;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnModificarCliente
             // 
--- Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/modificarCliente.cs
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/modificarCliente.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Diploma_HerminiaMarske_Noche_UAI_Lomas.forms;
+using Diploma_HerminiaMarske_Noche_UAI_Lomas.objetos;
+
 
 namespace Diploma_HerminiaMarske_Noche_UAI_Lomas.forms
 {
     public partial class modificarCliente : Form
     {
+        public static Object idCliente;
         public modificarCliente()
         {
             InitializeComponent();
@@ -20,7 +24,41 @@ namespace Diploma_HerminiaMarske_Noche_UAI_Lomas.forms
 
         private void modificarCliente_Load(object sender, EventArgs e)
         {
-            
+            int idClienteSelec = Int32.Parse((string)formInicio.idClienteModif);
+
+            SqlParameter[] pms = new SqlParameter[1];
+
+            pms[0] = new SqlParameter("@idCliente", SqlDbType.Int);
+            pms[0].Value = idClienteSelec;
+
+            DataConnection.DataConnection dataQuery = new DataConnection.DataConnection();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            da = dataQuery.getList("ObtenerCliente", pms);
+            da.Fill(dt);
+
+            DataRow dr = dt.Rows[0];
+           
+            Persona persona = new Persona((int)dr[4], (string)dr[5], (string)dr[6], (string)dr[7], (string)dr[8]);
+            Cliente cliente = new Cliente((int)dr[0], (string)dr[1], (string)dr[2], (string)dr[3], persona);
+
+            txtRazonSocial.Text = cliente.GetRazonSocial();
+            txtCuit.Text = cliente.GetCuit();
+            comboTipoCliente.Text = cliente.GetTipoCliente();
+            txtDni.Text = persona.GetDni();
+            txtNombre.Text = persona.GetNombre();
+            txtApellido.Text = persona.GetApellido();
+            comboSexo.Text = persona.GetSexo();
+            pickerFechaNacimiento.MinDate = persona.GetFechaNacimiento();
+
+           
+
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
