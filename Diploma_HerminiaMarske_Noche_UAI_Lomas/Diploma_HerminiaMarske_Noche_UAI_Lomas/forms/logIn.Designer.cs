ec05ad2fa750cccaa33e7383262806bc16eb4269
HermiMarske <35698704+HermiMarske@users.noreply.github.com>
2019-03-16 18:56:36 -0300

    modificado el coso de login

--- Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/logIn.Designer.cs
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/logIn.Designer.cs
@@ -46,18 +46,16 @@
             this.panel1.Controls.Add(this.btnIngresar);
             this.panel1.Controls.Add(this.btnSalir);
             this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Location = new System.Drawing.Point(6, 6);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Location = new System.Drawing.Point(5, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(490, 355);
+            this.panel1.Size = new System.Drawing.Size(436, 284);
             this.panel1.TabIndex = 0;
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(247, 282);
-            this.btnIngresar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnIngresar.Location = new System.Drawing.Point(220, 226);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(233, 62);
+            this.btnIngresar.Size = new System.Drawing.Size(207, 50);
             this.btnIngresar.TabIndex = 2;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -65,10 +63,9 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(7, 282);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSalir.Location = new System.Drawing.Point(6, 226);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(233, 62);
+            this.btnSalir.Size = new System.Drawing.Size(207, 50);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -80,57 +77,53 @@
             this.groupBox1.Controls.Add(this.lblContra);
             this.groupBox1.Controls.Add(this.txtUsuario);
             this.groupBox1.Controls.Add(this.lblUsr);
-            this.groupBox1.Location = new System.Drawing.Point(8, 9);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(472, 265);
+            this.groupBox1.Size = new System.Drawing.Size(420, 212);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de Ingreso";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(11, 185);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPassword.Location = new System.Drawing.Point(10, 148);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(454, 26);
+            this.txtPassword.Size = new System.Drawing.Size(404, 22);
             this.txtPassword.TabIndex = 3;
             // 
             // lblContra
             // 
             this.lblContra.AutoSize = true;
-            this.lblContra.Location = new System.Drawing.Point(7, 160);
+            this.lblContra.Location = new System.Drawing.Point(6, 128);
             this.lblContra.Name = "lblContra";
-            this.lblContra.Size = new System.Drawing.Size(92, 20);
+            this.lblContra.Size = new System.Drawing.Size(81, 17);
             this.lblContra.TabIndex = 2;
             this.lblContra.Text = "Contraseña";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(11, 70);
-            this.txtUsuario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUsuario.Location = new System.Drawing.Point(10, 56);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(454, 26);
+            this.txtUsuario.Size = new System.Drawing.Size(404, 22);
             this.txtUsuario.TabIndex = 1;
             // 
             // lblUsr
             // 
             this.lblUsr.AutoSize = true;
-            this.lblUsr.Location = new System.Drawing.Point(7, 45);
+            this.lblUsr.Location = new System.Drawing.Point(6, 36);
             this.lblUsr.Name = "lblUsr";
-            this.lblUsr.Size = new System.Drawing.Size(64, 20);
+            this.lblUsr.Size = new System.Drawing.Size(57, 17);
             this.lblUsr.TabIndex = 0;
             this.lblUsr.Text = "Usuario";
             // 
             // logIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 366);
+            this.ClientSize = new System.Drawing.Size(449, 293);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "logIn";
