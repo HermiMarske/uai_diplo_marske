f3ac4cb7df00eda3f5ca00f7414b9e118783efc7
emolinar <vzladrako.afz@gmail.com>
2019-05-13 09:28:33 -0300

    familias

---
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/altaFamilia.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Diploma_HerminiaMarske_Noche_UAI_Lomas.forms
+{
+    partial class altaFamilia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupFams = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.btnBorrarFam = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnModificarFam = new System.Windows.Forms.Button();
+            this.listboxFam = new System.Windows.Forms.ListBox();
+            this.listBoxPatXFam = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkedListPatentes = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listPatOtorgadas = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnCrearFam = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupFams.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(913, 836);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(907, 830);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnModificarFam);
+            this.tabPage1.Controls.Add(this.btnSalir);
+            this.tabPage1.Controls.Add(this.btnBorrarFam);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupFams);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(899, 797);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Familias";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(899, 797);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Agregar";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupFams
+            // 
+            this.groupFams.Controls.Add(this.listboxFam);
+            this.groupFams.Location = new System.Drawing.Point(6, 6);
+            this.groupFams.Name = "groupFams";
+            this.groupFams.Size = new System.Drawing.Size(435, 732);
+            this.groupFams.TabIndex = 0;
+            this.groupFams.TabStop = false;
+            this.groupFams.Text = "Familias";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBoxPatXFam);
+            this.groupBox2.Location = new System.Drawing.Point(458, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(435, 732);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Patentes por Familia";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(899, 797);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Modificar";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrarFam
+            // 
+            this.btnBorrarFam.Location = new System.Drawing.Point(6, 744);
+            this.btnBorrarFam.Name = "btnBorrarFam";
+            this.btnBorrarFam.Size = new System.Drawing.Size(283, 47);
+            this.btnBorrarFam.TabIndex = 2;
+            this.btnBorrarFam.Text = "Borrar";
+            this.btnBorrarFam.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(610, 744);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(283, 47);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarFam
+            // 
+            this.btnModificarFam.Location = new System.Drawing.Point(309, 744);
+            this.btnModificarFam.Name = "btnModificarFam";
+            this.btnModificarFam.Size = new System.Drawing.Size(283, 47);
+            this.btnModificarFam.TabIndex = 4;
+            this.btnModificarFam.Text = "Modificar Seleccionada";
+            this.btnModificarFam.UseVisualStyleBackColor = true;
+            // 
+            // listboxFam
+            // 
+            this.listboxFam.FormattingEnabled = true;
+            this.listboxFam.ItemHeight = 20;
+            this.listboxFam.Location = new System.Drawing.Point(6, 23);
+            this.listboxFam.Name = "listboxFam";
+            this.listboxFam.Size = new System.Drawing.Size(423, 704);
+            this.listboxFam.TabIndex = 0;
+            // 
+            // listBoxPatXFam
+            // 
+            this.listBoxPatXFam.FormattingEnabled = true;
+            this.listBoxPatXFam.ItemHeight = 20;
+            this.listBoxPatXFam.Location = new System.Drawing.Point(6, 23);
+            this.listBoxPatXFam.Name = "listBoxPatXFam";
+            this.listBoxPatXFam.Size = new System.Drawing.Size(423, 704);
+            this.listBoxPatXFam.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCerrar);
+            this.groupBox1.Controls.Add(this.btnCrearFam);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.listPatOtorgadas);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkedListPatentes);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(890, 785);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Crear Familia";
+            // 
+            // checkedListPatentes
+            // 
+            this.checkedListPatentes.FormattingEnabled = true;
+            this.checkedListPatentes.Location = new System.Drawing.Point(10, 115);
+            this.checkedListPatentes.Name = "checkedListPatentes";
+            this.checkedListPatentes.Size = new System.Drawing.Size(420, 613);
+            this.checkedListPatentes.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre de Familia";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(721, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Patentes disponibles";
+            // 
+            // listPatOtorgadas
+            // 
+            this.listPatOtorgadas.FormattingEnabled = true;
+            this.listPatOtorgadas.ItemHeight = 20;
+            this.listPatOtorgadas.Location = new System.Drawing.Point(464, 124);
+            this.listPatOtorgadas.Name = "listPatOtorgadas";
+            this.listPatOtorgadas.Size = new System.Drawing.Size(420, 604);
+            this.listPatOtorgadas.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Patentes otorgadas";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(601, 732);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(283, 47);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Salir";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCrearFam
+            // 
+            this.btnCrearFam.Location = new System.Drawing.Point(10, 732);
+            this.btnCrearFam.Name = "btnCrearFam";
+            this.btnCrearFam.Size = new System.Drawing.Size(283, 47);
+            this.btnCrearFam.TabIndex = 6;
+            this.btnCrearFam.Text = "Crear";
+            this.btnCrearFam.UseVisualStyleBackColor = true;
+            // 
+            // altaFamilia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 860);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "altaFamilia";
+            this.Text = "Familias";
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupFams.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupFams;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button btnModificarFam;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnBorrarFam;
+        private System.Windows.Forms.ListBox listBoxPatXFam;
+        private System.Windows.Forms.ListBox listboxFam;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCrearFam;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listPatOtorgadas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListPatentes;
+    }
+}
