29c89ce36096db185620ffc5292503080a1dc075
HermiMarske <35698704+HermiMarske@users.noreply.github.com>
2019-05-19 04:17:54 -0300

    la pantalla sin nada

---
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/bitacora.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Diploma_HerminiaMarske_Noche_UAI_Lomas.forms
+{
+    partial class bitacora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // bitacora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 769);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "bitacora";
+            this.Text = "Bitacora de Eventos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
