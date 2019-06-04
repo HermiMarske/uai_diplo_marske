925930c14fbf756f08ab713817dddb19f26a0cf2
HermiMarske <35698704+HermiMarske@users.noreply.github.com>
2019-06-03 23:44:45 -0300

    metodo de calcular dvv

--- Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/bitacora.Designer.cs
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/forms/bitacora.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboCriticidad = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.dateTimeFechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeFechaHasta = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
             this.btnFiltrar = new System.Windows.Forms.Button();
+            this.dateTimeFechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboCriticidad = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.dataGridBitacora = new System.Windows.Forms.DataGridView();
-            this.btnSalir = new System.Windows.Forms.Button();
             this.fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.criticidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dateTimeFechaHasta = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridBitacora)).BeginInit();
             this.SuspendLayout();
@@ -59,37 +59,59 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtUsuario);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(11, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1436, 64);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1276, 51);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Busqueda";
             // 
-            // label1
+            // dateTimeFechaHasta
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario";
+            this.dateTimeFechaHasta.Location = new System.Drawing.Point(980, 18);
+            this.dateTimeFechaHasta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimeFechaHasta.Name = "dateTimeFechaHasta";
+            this.dateTimeFechaHasta.Size = new System.Drawing.Size(185, 22);
+            this.dateTimeFechaHasta.TabIndex = 8;
             // 
-            // txtUsuario
+            // label4
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(76, 24);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(208, 26);
-            this.txtUsuario.TabIndex = 1;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(884, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha hasta";
             // 
-            // label2
+            // btnFiltrar
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 27);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Criticidad";
+            this.btnFiltrar.Location = new System.Drawing.Point(1182, 13);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(89, 34);
+            this.btnFiltrar.TabIndex = 6;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // dateTimeFechaDesde
+            // 
+            this.dateTimeFechaDesde.Location = new System.Drawing.Point(688, 18);
+            this.dateTimeFechaDesde.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimeFechaDesde.Name = "dateTimeFechaDesde";
+            this.dateTimeFechaDesde.Size = new System.Drawing.Size(185, 22);
+            this.dateTimeFechaDesde.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(592, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha desde";
             // 
             // comboCriticidad
             // 
@@ -98,36 +120,37 @@
             "ALTA",
             "MEDIA",
             "BAJA"});
-            this.comboCriticidad.Location = new System.Drawing.Point(370, 22);
+            this.comboCriticidad.Location = new System.Drawing.Point(329, 18);
+            this.comboCriticidad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboCriticidad.Name = "comboCriticidad";
-            this.comboCriticidad.Size = new System.Drawing.Size(208, 28);
+            this.comboCriticidad.Size = new System.Drawing.Size(185, 24);
             this.comboCriticidad.TabIndex = 3;
             // 
-            // label3
+            // label2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(666, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Fecha desde";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Criticidad";
             // 
-            // dateTimeFechaDesde
+            // txtUsuario
             // 
-            this.dateTimeFechaDesde.Location = new System.Drawing.Point(774, 22);
-            this.dateTimeFechaDesde.Name = "dateTimeFechaDesde";
-            this.dateTimeFechaDesde.Size = new System.Drawing.Size(208, 26);
-            this.dateTimeFechaDesde.TabIndex = 5;
+            this.txtUsuario.Location = new System.Drawing.Point(68, 19);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(185, 22);
+            this.txtUsuario.TabIndex = 1;
             // 
-            // btnFiltrar
+            // label1
             // 
-            this.btnFiltrar.Location = new System.Drawing.Point(1330, 16);
-            this.btnFiltrar.Name = "btnFiltrar";
-            this.btnFiltrar.Size = new System.Drawing.Size(100, 42);
-            this.btnFiltrar.TabIndex = 6;
-            this.btnFiltrar.Text = "Filtrar";
-            this.btnFiltrar.UseVisualStyleBackColor = true;
-            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
             // 
             // dataGridBitacora
             // 
@@ -138,66 +161,59 @@
             this.criticidad,
             this.descripcion,
             this.usuario});
-            this.dataGridBitacora.Location = new System.Drawing.Point(12, 82);
+            this.dataGridBitacora.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridBitacora.Location = new System.Drawing.Point(10, 69);
+            this.dataGridBitacora.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridBitacora.Name = "dataGridBitacora";
+            this.dataGridBitacora.ReadOnly = true;
             this.dataGridBitacora.RowTemplate.Height = 28;
-            this.dataGridBitacora.Size = new System.Drawing.Size(1436, 817);
+            this.dataGridBitacora.ShowCellToolTips = false;
+            this.dataGridBitacora.ShowEditingIcon = false;
+            this.dataGridBitacora.Size = new System.Drawing.Size(1276, 654);
             this.dataGridBitacora.TabIndex = 1;
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Location = new System.Drawing.Point(1145, 909);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(301, 40);
-            this.btnSalir.TabIndex = 2;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // fecha
             // 
             this.fecha.HeaderText = "Fecha";
             this.fecha.Name = "fecha";
+            this.fecha.ReadOnly = true;
             // 
             // criticidad
             // 
             this.criticidad.HeaderText = "Criticidad";
             this.criticidad.Name = "criticidad";
+            this.criticidad.ReadOnly = true;
             // 
             // descripcion
             // 
             this.descripcion.HeaderText = "Descripcion";
             this.descripcion.Name = "descripcion";
+            this.descripcion.ReadOnly = true;
             // 
             // usuario
             // 
             this.usuario.HeaderText = "Usuario";
             this.usuario.Name = "usuario";
+            this.usuario.ReadOnly = true;
             // 
-            // dateTimeFechaHasta
+            // btnSalir
             // 
-            this.dateTimeFechaHasta.Location = new System.Drawing.Point(1103, 22);
-            this.dateTimeFechaHasta.Name = "dateTimeFechaHasta";
-            this.dateTimeFechaHasta.Size = new System.Drawing.Size(208, 26);
-            this.dateTimeFechaHasta.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(994, 26);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 20);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Fecha hasta";
+            this.btnSalir.Location = new System.Drawing.Point(1018, 727);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(268, 32);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // bitacora
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1458, 961);
+            this.ClientSize = new System.Drawing.Size(1296, 769);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.dataGridBitacora);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "bitacora";
---
+++ Diploma_HerminiaMarske_Noche_UAI_Lomas/Diploma_HerminiaMarske_Noche_UAI_Lomas/servicio/ControladorDigitosVerificadores.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diploma_HerminiaMarske_Noche_UAI_Lomas.servicio
+{
+    class ControladorDigitosVerificadores
+    {
+        public static int calcularDVV(String entrada)
+        {
+            int salida = 0;
+
+            entrada.Trim();
+
+            char[] entradaParaProcesar = entrada.ToCharArray();
+
+            int[] entradaArray = {0};
+
+            for(int i=0; i<=entrada.Length; i++)
+            {
+                entradaArray[i] = Convert.ToInt32(new string(entradaParaProcesar[i], 1)) * i;
+            }
+
+            for(int i=0; i<=entradaArray.Length; i++)
+            { 
+                salida += entradaArray[i];
+            }
+
+            return salida;
+        }
+
+
+
+        public static void calcularDVH()
+        {
+
+        }
+
+        public static void recalcularDV()
+        {
+
+        }
+
+        
+    }
+
+
+}
